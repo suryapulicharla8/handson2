6da68c39a3223c1e511a6ab7a9f34f65d89428c9
suryapulicharla8 <suryapulicharla8@gmail.com>
2023-03-10 11:31:13 +0530

    second

--- src/App.js
+++ src/App.js
@@ -39,19 +39,19 @@ export default class App extends Component {
         <div className='formdiv'>
           <form>
             
-            <lable htmlFor='Name'>Name:</lable>
-            <input className='name' name='Name' type='text' value={this.state.Name} onChange={this.changeHandler}></input>
+            <lable id='name' htmlFor='Name'>Name:</lable>
+            <input  className='name' name='Name' type='text' value={this.state.Name} onChange={this.changeHandler}></input>
             <br/>
-            <lable htmlFor='Department'>Department:</lable>
+            <lable id='name' htmlFor='Department'>Department:</lable>
             <input type="text" className='name' name="Department" value={this.state.Department} onChange={this.changeHandler}></input>
             <br/>
-            <lable htmlFor='Rating'>Rating:</lable>
+            <lable id='name' htmlFor='Rating'>Rating:</lable>
             <input type="number" className='name' name='Rating' value={this.state.Rating} onChange={this.changeHandler}></input>
             <br/>
             <button className='btn' onClick={this.submitHandler}>Submit</button>
           </form>
           <div className='flex'>
-            {this.state.employees.map((value,index)=>{
+            {this.state.employees.map((value)=>{
               return(
                   <div className='box'>
                     Name:{value.Name} | Department: {value.Department} | Rating:{value.Rating}
